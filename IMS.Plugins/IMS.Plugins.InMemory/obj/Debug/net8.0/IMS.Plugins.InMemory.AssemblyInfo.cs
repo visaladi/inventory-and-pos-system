193d6b6c0177c4e5822144902c10dad1b42f0692
S. T. Jayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2023-12-14 22:09:51 +0530

    solved edit button bug

--- IMS.Plugins/IMS.Plugins.InMemory/obj/Debug/net8.0/IMS.Plugins.InMemory.AssemblyInfo.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/obj/Debug/net8.0/IMS.Plugins.InMemory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.Plugins.InMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e5eada549f293d9f0c3f712e45855cb88e7753")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.Plugins.InMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.Plugins.InMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
