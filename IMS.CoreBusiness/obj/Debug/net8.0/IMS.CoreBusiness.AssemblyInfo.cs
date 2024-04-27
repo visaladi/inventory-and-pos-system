570ff1b9b1af921486bbc33939c57e18064f1344
SupunTJ <130586987+SupunJayaweera@users.noreply.github.com>
2024-04-27 10:35:26 +0530

    initializing new branch

--- IMS.CoreBusiness/obj/Debug/net8.0/IMS.CoreBusiness.AssemblyInfo.cs
+++ IMS.CoreBusiness/obj/Debug/net8.0/IMS.CoreBusiness.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.CoreBusiness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c1fbca0a8ba2ad7465eb6746f6100d34527e37e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49de661e6f042828c0fdd5260ae0e25ecfc468a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.CoreBusiness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.CoreBusiness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IMS.UseCases/obj/Debug/net8.0/IMS.UseCases.AssemblyInfo.cs
+++ IMS.UseCases/obj/Debug/net8.0/IMS.UseCases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.UseCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c1fbca0a8ba2ad7465eb6746f6100d34527e37e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49de661e6f042828c0fdd5260ae0e25ecfc468a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.UseCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.UseCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
