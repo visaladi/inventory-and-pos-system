9da8df970e3db00590dd993106d8c603784735c9
SupunTJ <jayaweeraspn@gmail.com>
2024-05-14 08:49:04 +0530

    separating the code and UI

---
+++ IMS.WebApp/Components/Pages/Activities/SellProduct.razor.cs
@@ -0,0 +1,59 @@
+﻿using IMS.CoreBusiness;
+using IMS.WebApp.ViewModels;
+using Microsoft.JSInterop;
+using static IMS.WebApp.Controls.Common.AutoCompleteComponent;
+
+namespace IMS.WebApp.Components.Pages.Activities
+{
+    public partial class SellProduct
+    {
+        private SellViewModel sellViewModel = new SellViewModel();
+
+        private Product? selectedProduct = null;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender)
+            {
+                JSRuntime.InvokeVoidAsync("PreventFormSubmition", "sell-form");
+            }
+        }
+
+        private async Task Sell()
+        {
+            if (this.selectedProduct != null)
+            {
+                await SellProductUseCase.ExecuteAsync(
+                    this.sellViewModel.SalesOrderNumber,
+                    this.sellViewModel.Product,
+                    this.sellViewModel.QuantityToSell,
+                    this.sellViewModel.UnitPrice
+                    // "Frank" // Doneby
+                    );
+            }
+
+            this.sellViewModel = new SellViewModel();
+            this.selectedProduct = null;
+        }
+
+        public async Task<List<ItemViewModel>?> SearchProduct(string name)
+        {
+            var list = await ViewProductsByNameUseCase.ExecuteAsync(name);
+
+            if (list == null) return null;
+
+            return list.Select(x => new ItemViewModel { Id = x.ProductID, Name = x.ProductName })?.ToList();
+        }
+
+        private async Task OnItemSelected(ItemViewModel item)
+        {
+            this.selectedProduct = await ViewProductByIdUseCase.ExecuteAsync(item.Id);
+
+            sellViewModel.ProductId = item.Id;
+            sellViewModel.Product = this.selectedProduct;
+            sellViewModel.UnitPrice = this.selectedProduct.Price;
+        }
+    }
+}
---
+++ IMS.WebApp/Components/Pages/Branch_POS/ProductsToCart.razor.cs
@@ -0,0 +1,90 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.WebApp.Components.Pages.Branch_POS
+{
+    public partial class ProductsToCart
+    {
+        private string email = "";
+        private IEnumerable<Product> products;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            email = authState.User.Identity?.Name;
+
+            products = (await ViewProductsByNameUseCase.ExecuteAsync("")).ToList();
+        }
+
+
+        private async Task AddToCart(int productId)
+        {
+            #region AddToCart_Test
+            // var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            // var userId = authState.User.Identity?.Name; // Get signed-in user's ID
+
+            // if (userId != null)
+            // {
+            //     await ShoppingCartService.AddItemToCartAsync(userId, productId, 1); // Add 1 unit by default
+            // }
+            // else
+            // {
+            //     // Handle scenario where user is not signed in (e.g., display a message or redirect to login)
+            // }
+
+            // var user = await UserManager.GetUserAsync(await AuthenticationStateProvider.GetAuthenticationStateAsync());
+
+
+
+
+            // var user = await UserManager.GetUserAsync(await AuthenticationStateProvider.GetAuthenticationStateAsync());
+            // var userId = user?.Id;
+            // // var user = await UserManager.GetUserAsync(HttpContext.User);
+            // // var userId = user?.FindFirstValue(ClaimTypes.NameIdentifier); // Assuming UserId is stored in NameIdentifier claim
+
+            // if (userId != null)
+            // {
+            //     await ShoppingCartService.AddItemToCartAsync(userId, productId, 1);
+            // }
+            // else
+            // {
+            //     // Handle scenario where user is not signed in
+            // }
+            #endregion
+
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = await UserManager.GetUserAsync(authState.User);
+            var userId = user?.Id;
+
+            if (userId != null)
+            {
+                await ShoppingCartService.AddItemToCartAsync(userId, productId, 1);
+            }
+            else
+            {
+            }
+
+            NavigationManager.NavigateTo($"/usercart/{userId}");
+        }
+
+        #region GetUserEmail
+
+        // private string GetUserEmail()
+        // {
+        // var claimsPrincipal = await HttpContext.GetCurrentUser();
+        // if (claimsPrincipal?.Identity?.IsAuthenticated == true)
+        // {
+        //     return claimsPrincipal.FindFirstValue(ClaimTypes.Email);
+        // }
+        // return "";
+
+
+        // if (HttpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false)
+        // {
+        //     return httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Email);
+        // }
+
+        // return "Not Signed In";
+        // }
+        #endregion
+    }
+}
