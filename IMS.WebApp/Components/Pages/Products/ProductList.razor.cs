652e414aec2d674f1be2088f01f671ac3bb0e39b
SupunTJ <jayaweeraspn@gmail.com>
2024-05-16 14:28:39 +0530

    Added code separation for Product pages

---
+++ IMS.WebApp/Components/Pages/Products/ProductList.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace IMS.WebApp.Components.Pages.Products
+{
+    public partial class ProductList
+    {
+        // private List<Product> products = new List<Product>();
+
+        private string searchTerm = string.Empty;
+
+        private void OnSearchProduct(string searchTerm)
+        {
+            this.searchTerm = searchTerm;
+        }
+
+        private void AddProduct()
+        {
+            NavigationManager.NavigateTo("/addproduct");
+        }
+    }
+}
